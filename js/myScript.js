e92c2bd8a44df0f24482e85ef1e502330bf939e7
Daniela Deicmane (student) <335364@edu.mkcollege.ac.uk>
2024-11-08 12:27:33 +0000

    Added benefits section in homepage

--- js/myScript.js
+++ js/myScript.js
@@ -1,17 +1,73 @@
 //This function will be executed once the user clicks on the menu icon
 function showSidebar(){
     //Displays the menu items
-    const sideBar = document.querySelector(".menu-list")
-    sideBar.style.display = "flex"
+    const sideBar = document.querySelector(".menu-list");
+    sideBar.style.display = "flex";
 
     //Displays the "X" at the top to hidde the navigation
-    const exit = document.querySelector(".bx-x")
-    exit.style.display = "flex"
+    const exit = document.querySelector(".bx-x");
+    exit.style.display = "flex";
 }
 
 //This function will be executed once the user clicks on the "X"
 function hideSidebar(){
     //Hiddes the menu items
-    const noSideBar = document.querySelector(".menu-list")
-    noSideBar.style.display = "none"
+    const noSideBar = document.querySelector(".menu-list");
+    noSideBar.style.display = "none";
+}
+
+function benefit1(){
+    const addImage = document.querySelector("#benefit-image-1");
+    addImage.style.display = "flex";
+
+    const removeImage2 = document.querySelector("#benefit-image-2");
+    removeImage2.style.display = "none";
+
+    const removeImage3 = document.querySelector("#benefit-image-3");
+    removeImage3.style.display = "none";
+
+    const removeImage4 = document.querySelector("#benefit-image-4");
+    removeImage4.style.display = "none";
+}
+
+function benefit2(){
+    const removeImage1 = document.querySelector("#benefit-image-1");
+    removeImage1.style.display = "none";
+
+    const removeImage3 = document.querySelector("#benefit-image-3");
+    removeImage3.style.display = "none";
+
+    const addImage = document.querySelector("#benefit-image-2");
+    addImage.style.display = "flex";
+
+    const removeImage4 = document.querySelector("#benefit-image-4");
+    removeImage4.style.display = "none";
+}
+
+function benefit3(){
+    const addImage = document.querySelector("#benefit-image-3");
+    addImage.style.display = "flex";
+
+    const removeImage1 = document.querySelector("#benefit-image-1");
+    removeImage1.style.display = "none";
+
+    const removeImage2 = document.querySelector("#benefit-image-2");
+    removeImage2.style.display = "none";
+
+    const removeImage4 = document.querySelector("#benefit-image-4");
+    removeImage4.style.display = "none";
+}
+
+function benefit4(){
+    const addImage = document.querySelector("#benefit-image-4");
+    addImage.style.display = "flex";
+
+    const removeImage1 = document.querySelector("#benefit-image-1");
+    removeImage1.style.display = "none";
+
+    const removeImage2 = document.querySelector("#benefit-image-2");
+    removeImage2.style.display = "none";
+
+    const removeImage3 = document.querySelector("#benefit-image-3");
+    removeImage3.style.display = "none";
 }
--- view/index.php
+++ view/index.php
@@ -35,5 +35,41 @@
         <span>275,009 students</span>
     </div>
 
+    <div id="what-we-offer">
+        <div id="what-we-offer-img">
+            <img id="benefit-image-1" src="/static/online-science-tutoring-abstract-concept-illustration-personalised-learning-online-educational-platform-homeschooling.png" alt="Online science class">
+            <img id="benefit-image-2" src="/static/rb_40416.png" alt="Person with certificate">
+            <img id="benefit-image-3" src="/static/rb_1357.png" alt="Trusted by parents">
+            <img id="benefit-image-4" src="/static/rb_2149247168.png" alt="Gamification">
+        </div>
+
+        <div id="what-we-offer-text">
+            <div id="what-we-offer-top">
+                <div class="benefit-1" tabindex="1" onclick="benefit1()">
+                    <h1>Interactive Lessons</h1>
+                    <p>Learn faster thorugh interactive lessons</p>
+                </div>
+                <div class="benefit-2" tabindex="2" onclick="benefit2()">
+                    <h1>Certificated Tutors</h1>
+                    <p>All of our tutors are certificated</p>
+                </div>
+            </div>
+            
+            <div id="what-we-offer-bottom">
+                <div class="benefit-3" tabindex="3" onclick="benefit3()">
+                    <h1>Trusted by Parents</h1>
+                    <p>Some more text</p>
+                </div>
+                <div class="benefit-4" tabindex="4" onclick="benefit4()">
+                    <h1>Gamification</h1>
+                    <p>Some more text</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--Links to the JavaScript file-->
+    <script src="/js/myScript.js"></script>
+
 </body>
 </html>

512d7bbd19c704ac381597af1a9b4758513e4247
Daniela Deicmane (student) <335364@edu.mkcollege.ac.uk>
2024-11-12 17:00:54 +0000

    Initial code

--- view/sign-up-student.php
+++ view/sign-up-student.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign Up</title>
+    <title>Student Sign Up</title>
 
     <!--Download document for the icons-->
     <link rel="stylesheet"
@@ -15,50 +15,78 @@
 </head>
 <body>
 
-    <div>
-        <div>
+    <div class="sign-container">
+        <div class="sign-form">
+            <div>
+                <a href="index.php"><img class="sign-logo" src="/static/202129597-removebg-preview.png" alt="Logo"></a>
+            </div>
+            <h1>Student Sign Up</h1>
             <!--Uses post method to send user input to the php file more securely
             since the input will not appear on the search bar-->
-            <form action="#" method="post">
+            <form class="sign-input-fields" action="#" method="post">
+
+                <!--Option to sign up with google-->
+                <button class="sign-google" style="submit">Sign Up with Google</button>
+                <h2 class="line-with-text"><span>OR</span></h2>
+
                 <!--Input field for forename-->
-                <div>
+                <div class="input-field">
                     <!--Adds a label for the input field for accessibility (when using screen readers)-->
                     <label for="forename">Forename</label>
                     <input type="text" id="forename" name="forename">
                 </div>
+
                 <!--Input field for surname-->
-                <div>
+                <div class="input-field">
                     <!--Adds a label for the input field for accessibility (when using screen readers)-->
                     <label for="surname">Surname</label>
                     <input type="text" id="surname" name="surname">
                 </div>
+
                 <!--Input field for date of birth-->
-                <div>
+                <div class="input-field">
                     <!--Adds a label for the input field for accessibility (when using screen readers)-->
                     <label for="date-of-birth">Date of Birth</label>
                     <input type="date" id="date-of-birth" name="date of birth">
                 </div>
+
                 <!--Input field for email address-->
-                <div>
+                <div class="input-field">
                     <!--Adds a label for the input field for accessibility (when using screen readers)-->
                     <label for="email-address">Email Address</label>
                     <input type="email" id="email-address" name="email adress">
                 </div>
+
                 <!--Input field for password-->
-                <div>
+                <div class="input-field">
                     <!--Adds a label for the input field for accessibility (when using screen readers)-->
                     <label for="password">Password</label>
                     <input type="password" id="passwword" name="password">
                 </div>
+
                 <!--Input field for re-entering password-->
-                <div>
+                <div class="input-field">
                     <!--Adds a label for the input field for accessibility (when using screen readers)-->
                     <label for="re-password">Re-enter Password</label>
                     <input type="password" id="re-password" name="repeated password">
                 </div>
+
+                <!--Button to submit the inputed data-->
+                <button class="sign-up-button" type="submit" name="sign-up">Sign Up</button>
+                <!--Enables the user to go to the sign in page if they have an account, therefore improving UX-->
+                <p class="have-an-account">Have an account: <a class="sign-link-redirect" href="sign-in-student.php">Sign In</a></p>
+
             </form>
+            <!--Link to back to home that will send the user to the homepage-->
+            <div>
+                <a class="back-to-home" href="index.php">BACK TO HOME</a>
+            </div>
         </div>
-    <div>
+        <!--Image-->
+        <div class="sign-image">
+            <img src="/static/8899746.jpg" alt="Student Reading a Book">
+        </div>
+    </div>
 
 </body>
 </html>
